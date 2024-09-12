a31c0d8808669fa10caf08e8eb551a1e12c40ec6
Carlos Manuel Gamboa Palomino <gamboapalominocarlosmanuel@gmail.com>
2024-09-12 15:08:01 -0500

    NTS

--- resources/views/components/dashboard/top10.blade.php
+++ resources/views/components/dashboard/top10.blade.php
@@ -105,7 +105,7 @@
           $('#topProducts').append(`<tr>
             <td class="p-2">
               <div class="flex items-center">
-                <img class="object-center object-contain rounded-md me-2 border w-[32px] aspect-square" src="${row.image ? `/${row.image}`: '/images/img/noimagen.jpg'}" alt="Imagen del producto">
+                <img class="object-center object-contain rounded-md me-2 w-[32px] aspect-square" src="${row.image ? `/${row.image}`: '/images/img/noimagen.jpg'}" alt="Imagen del producto">
                 <div class="text-slate-800 dark:text-slate-100">${row.name}
                   ${row.color ? `- ${row.color}`: ''}
                 </div>
