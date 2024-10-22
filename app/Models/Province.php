b205c72cf7ce9fd8b9b35bf6f6af5a89f69600ae
Carlos Colina <carlosecolina89@gmail.com>
2024-10-22 10:47:46 -0500

    act db

--- app/Models/Province.php
+++ app/Models/Province.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Province extends Model
 {
     use HasFactory;
+    public $incrementing = false;
+    protected $keyType = 'string';
     
     protected $fillable = [        
         'department_id',
---
+++ database/migrations/2024_10_22_153343_change_colum_tipe.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        Schema::table('prices', function (Blueprint $table) {
+            //
+            $table->char('distrito_id')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+        Schema::table('prices', function (Blueprint $table) {
+            $table->bigInteger('distrito_id')->change();
+
+        });
+    }
+};
