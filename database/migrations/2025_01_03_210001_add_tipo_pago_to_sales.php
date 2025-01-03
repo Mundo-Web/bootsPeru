e935f9de5d3481c64b7d06744fa443e013f11101
gysmoy <gamboapalominocarlosmanuel@gmail.com>
2025-01-03 16:10:37 -0500

    NTS

--- database/migrations/2025_01_03_210001_add_tipo_pago_to_sales.php
+++ database/migrations/2025_01_03_210001_add_tipo_pago_to_sales.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('sales', function (Blueprint $table) {
-            $table->enum('tipo_pago', ['culqi', 'transferencia', 'billetera']);
+            $table->enum('tipo_pago', ['culqi', 'transferencia', 'billetera'])->nullable();
         });
     }
 
