3c94a0310729ae1c00bd14efef640852d2ed7636
gysmoy <gamboapalominocarlosmanuel@gmail.com>
2025-01-08 11:21:19 -0500

    NTS

--- resources/views/components/sales/modal.blade.php
+++ resources/views/components/sales/modal.blade.php
@@ -3,7 +3,7 @@
   $statuses = isset($statuses) ? $statuses : [];
 @endphp
 
-<div id="invoice-modal" class="modal !max-w-[720px] relative">
+<div id="invoice-modal" class="modal !max-w-[820px] relative">
   @csrf
   <input type="hidden" id="invoice-id" value="">
   <div class="relative md:absolute border rounded-lg right-8 top-6 py-2 px-3 mb-2 text-center">
@@ -14,23 +14,23 @@
       class="w-max block mx-auto text-xs font-medium px-2.5 py-0.5 mb-1 rounded-full"></span> --}}
   </div>
   <h4 class="h4 mb-2 mt-2">Orden #<span id="invoice-code"></span></h4>
-  <p id="invoice-client" class="font-bold mb-2"></p>
-  <span>Direccion Envio:</span>
-  <p id="invoice-address" class="text-gray-700 mb-2"></p>
-  <span>Telefono: </span>
-  <p id="invoice-telefono" class="text-gray-700 mb-2"></p>
+  <p id="invoice-client" class="mb-2 font-bold"></p>
+  <div class="flex flex-row gap-2"><span>Email: </span><p id="email-client"></p></div>
+  <div class="flex flex-row gap-2"><span>Dni/Ruc: </span><p id="dni-client" class=""></p></div>
+  <div class="flex flex-row gap-2"><span>Telefono: </span><p id="phone-client" class=""></p></div>
+  <div class="flex flex-row gap-2"><span>Direccion Envio:</span>
+  <p id="invoice-address" class="text-gray-700 mb-2"></p></div>
 
-  <p class="font-bold"> Datos Facturacion: </p>
-  <p class=" ">
-    <span>Nombre / Razon Social: </span>
-    <span id="razonS"></span>
-  </p>
-  <p class=" mb-2">
-    <span> Direccion Fiscal:</span>
-    <span id="dirFact"></span>
-  </p>
-  <div id="containerTRansferencia">
-
+  <div id="seccioncomprobante" class="flex flex-col gap-2">
+    <p class="font-bold"> Datos Facturacion: </p>
+    <p id="razonsocial">
+      <span>Nombre / Razon Social: </span>
+      <span id="razonS"></span>
+    </p>
+    <p id="direccion" class="mb-2">
+      <span> Direccion Fiscal:</span>
+      <span id="dirFact"></span>
+    </p>
   </div>
 
   @if ($isAdmin)
@@ -69,7 +69,6 @@
       <div
         class="relative w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600">
       </div>
-
       <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">
         @if (!$isAdmin)
           Productos entregados
@@ -106,25 +105,43 @@
 </div>
 
 <script>
-  // $(document).on('click', '#invoice-modal .close-modal', function() {
-  //   console.log('close')
-  //   $('#invoice-modal').modal('close')
-  // })
   const isAdmin = {{ $isAdmin }};
   const openSaleModal = (data) => {
     const isFree = !Boolean(Number(data.address_price))
     const envio = data.address_price
-
-    console.log(data)
+    const idcupon = data.idcupon
+    const montocupon = data.cupon_monto ?? 0
+    const nombrecupon = data.cupon?.codigo ?? "sin cupon"
+    
 
     $('#invoice-id').val(data.id)
     $('#address-tipo-comprobante').text(data.tipo_comprobante.toUpperCase())
     $('#n_document').text(data.doc_number)
-    $('#razonS').text(data.razon_fact)
-    $('#dirFact').text(data.direccion_fact)
+
+
+      if (!data.razon_fact && !data.direccion_fact) {
+          $('#seccioncomprobante').hide();
+      } else { 
+          $('#seccioncomprobante').show();
+
+      if (data.razon_fact) {
+          $('#razonS').text(data.razon_fact);
+          $('#razonsocial').show(); 
+      } else {
+          $('#razonsocial').hide(); 
+      }
+
+      if (data.direccion_fact) {
+          $('#dirFact').text(data.direccion_fact);
+          $('#direccion').show(); 
+      } else {
+          $('#direccion').hide(); 
+      }
+    }
+
+
     let totalInvoice = Number(data.total) + Number(envio)
     $('#invoice-price').text(totalInvoice)
-    $('#invoice-telefono').text(data.phone ?? '')
     /* $('#invoice-address-price').text(isFree ? 'Envio gratis' :
       `S/. ${Number(data.address_price).toFixed(2)}`) */
     if (isFree) $('#invoice-address-price')
@@ -135,6 +152,9 @@
       .removeClass('bg-green-100 text-green-800 dark:bg-green-900 dark:text-green-300')
     $('#invoice-code').text(data.code)
     $('#invoice-client').text(`${data.name} ${data.lastname}`)
+    $('#email-client').text(`${data.email}`)
+    $('#dni-client').text(`${data.doc_number}`)
+    $('#phone-client').text(`${data.phone}`)
     $('#invoice-address').text(data.address_description ?
       `${data.address_department}, ${data.address_province}, ${data.address_district} - ${data.address_street} #${data.address_number}` :
       'Recojo en tienda')
@@ -158,21 +178,6 @@
         <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-8"></div>
       </td>
     </tr>`)
-    if (data.img_transferencia) {
-      var enlace = document.createElement('a');
-      let extencion = data.img_transferencia.split('.').pop();
-      console.log(extencion)
-      enlace.className = 'underline pb-6';
-      enlace.href = `/${data.img_transferencia}`;
-      enlace.download = `transferencia.${extencion}`;
-      enlace.target = '_blank';
-      enlace.textContent = 'Img Transferencia';
-
-      // Insertar el enlace dentro del div con el ID containerTRansferencia
-      document.getElementById('containerTRansferencia').appendChild(enlace);
-    }
-
-
     fetch(`/api/saledetails/${data.id}`)
       .then(res => res.json())
       .then(data => {
@@ -185,23 +190,59 @@
           </tr>`)
           return
         }
-        console.log(data)
         data.forEach(item => {
-          $('#invoice-products').append(`<tr class="bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
-            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-              ${item.product_name} 
-            </th>
-            <td class="px-6 py-4">
-              S/. ${Number(item.price).toFixed(2)}
-            </td>
-            <td class="px-6 py-4">
-              ${item.quantity}
-            </td>
-            <td class="px-6 py-4">
-              S/. ${(item.price * item.quantity).toFixed(2)}
-            </td>
-          </tr>`)
-        })
+          const shouldStrikePrice = (item.price * item.quantity) > item.final_price;
+          const unitPrice = Number(item.final_price / item.quantity).toFixed(2);
+          const discountPercentage = shouldStrikePrice
+          ? ((1 - (item.final_price / (item.price * item.quantity))) * 100).toFixed(2)
+          : null;
+
+          $('#invoice-products').append(`
+                <tr class="bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+                    <th scope="row" class="px-6 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        ${item.product_name} <br>
+                        <span class="text-xs font-bold">
+                          COLOR: <span class="text-xs font-normal">${item.product_color}</span><br> 
+                          TALLA: <span class="text-xs font-normal">${item.talla}</span><br>
+                          MARCA: <span class="text-xs font-normal">${item.marca ? item.marca : "Sin Marca"}</span>
+                        </span>
+                    </th>
+                    <td class="px-6 py-2">
+                        ${shouldStrikePrice
+                            ? `<s class="text-xs">S/. ${Number(item.price).toFixed(2)}</s><br>S/. ${unitPrice}<br> 
+                              <span class="text-green-500 text-xs text-red-500">(${discountPercentage}% dcto)</span>`
+                            : `S/. ${Number(item.price).toFixed(2)}`
+                        }
+                    </td>
+                    <td class="px-6 py-2">
+                        ${item.quantity}
+                    </td>
+                    <td class="px-6 py-2">
+                        S/. ${Number(item.final_price).toFixed(2)}
+                    </td>
+                </tr>
+            `)
+        });
+
+        if (montocupon > 0) {
+            $('#invoice-products').append(`
+                <tr class="bg-white text-red-600 border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+                    <th scope="row" class="text-red-600 px-6 py-4 font-medium  whitespace-nowrap dark:text-white">
+                        Cupón (${nombrecupon}) <i class="fa-solid fa-ticket"></i>
+                    </th>
+                    <td class="px-6 py-4">
+                        - S/. ${montocupon}
+                    </td>
+                    <td class="px-6 py-4">
+                        1
+                    </td>
+                    <td class="px-6 py-4">
+                        - S/. ${montocupon}
+                    </td>
+                </tr>
+            `);
+        }
+
         $('#invoice-products').append(`<tr class="bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
             <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
               Envio 
@@ -216,6 +257,7 @@
               S/. ${envio}
             </td>
           </tr>`)
+          
       })
 
     $('#invoice-modal').modal('show')
